c2d346926d74c570550593dc15ba47282948537b
DMZ <dmz@DMZs-MBP.attlocal.net>
2022-07-08 17:05:56 -0500

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,14 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        while (x < 10) {
+            sum += x;
+            System.out.print(sum + " ");
+            x += 1;
+        }
+        System.out.println("\n");
+        System.out.println(5 + "10");
+        System.out.println(5 + 10);
+    }
+}
